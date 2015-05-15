6aec82388b878b07fea57e697ea6858d5f017bfb
感性情報学研究室 <KANSEIINFO@kansei.local>
2015-05-15 11:57:22 +0900

    single_click_Link_function

--- background.js
+++ background.js
@@ -7,6 +7,7 @@ chrome.browserAction.onClicked.addListener(function() {
 
     if (connected) {
       //Leap.loop({ enableGestures: true }, onframe);
+      console.log("接続完了！");
         leap = new Leap.Controller({
                   //host: "127.0.0.1",
                   //port: 6437,
@@ -23,94 +24,90 @@ chrome.browserAction.onClicked.addListener(function() {
 
 function onframe(frame) {
     if (!connected) {
+        console.log("接続失敗");
         return;
     }
     var i = 0
-    var iz = frame.gestures.length;
+    var iges = frame.gestures.length;
     var ifin = frame.fingers.length;
     var extendedFingers = 0;
 
-    if(iz > 0){
-    for (; i < iz; ++i) {
-        var gesture = frame.gestures[i];
+    if(iges > 0){
+        for (; i < iges; ++i) {
+            var gesture = frame.gestures[i];
 
-        if (gesture.type === "swipe") {
-                switch (gesture.state) {
-                case "start":
-                    if (gestureState === 0) { // avoid chattering
-                        gestureState = 1;
-                    }
-                    break;
-                case "update":
-                    if (gestureState === 1) { // avoid chattering
-                        gestureState = 2;
-                    }
-                    break;
-                case "stop":
-                    if (gestureState === 2) { // avoid chattering
-                        gestureState = 0;
-                        swipe(  gesture.direction[0],
-                                gesture.direction[1],
-                                gesture.direction[2]);
-                    }
+            if(gesture.type === "keyTap" || gesture.type === "screenTap") {
+                clickToLink();             
+            }
+
+            if(gesture.type === "swipe"){
+                switch(gesture.state){
+                    case "start":
+                        if(gestureState === 0){
+                            gestureState = 1;
+                        }
+                        break;
+                    case "update":
+                        if(gestureState === 1){
+                            gestureState = 2;
+                        }
+                        break;
+                    case "stop":
+                        if(gestureState === 2){
+                            gestureState = 0;
+                            swipe(  gesture.direction[0],
+                                    gesture.direction[1],
+                                    gesture.direction[2]);
+                        }
                 }
             }
         }
     }
 
-    if(ifin > 0){//指が検出されたら、、、
+/*
+    //if(ifin > 0){指が検出されたら、、、
         var hand_data = frame.hands[i];
         var finger_data = frame.fingers[i];
         var pointable_data = frame.pointables[i];
-        
+        console.log('ここから、追加情報');
         var nameMap = ["thumb", "index","middle", "ring", "pinky"];
         var fingersName = nameMap[finger.type];
 
         for(var j=0; j < ifin; ++j){
-            if(fingers.extended && ifin == 5){//指が開いているかつ、検出された指の数が5のとき。
                 console.log(gesture.pointableIds);
-                switch(gesture.pointableIds){
-                    //各指の検出って、switchの番号振り分けで可能なのだろうか
-                    case 0:
-                        if(gesture.type === "keytap" || gesture.type === "screentap"){
-                            
-                        }
-                    break;
-                    case 1:
-                        if(gesture.type === "keytap" || gesture.type === "screentap"){
-                            
-                        }
-                    break;
-                    case 2:
-                        if(gesture.type === "keytap" || gesture.type === "screentap"){
-                            
-                        }
-                    break;
-                    case 3:
-                        if(gesture.type === "keytap" || gesture.type === "screentap"){
-                            
-                        }
-                    break;
-                    case 4:
-                        if(gesture.type === "keytap" || gesture.type === "screentap"){
-                            
-                        }
-                    break;
-                }
                 
-            }
+                //if(finger.type === 0){
+                //fingersName = nameMap[finger.type];
+                    console.log(fingersName);
+                    if(gesture.type === 'keytap' || gesture.type === 'screentap'){
+                        console.log('ここから、追加情報');
+                        console.log(gesture.pointableIds);
+                    }
+                //}
         }
-    }
+    //}
+*/
+
 }
+
+
 
 function swipe(x, y, z) {
     if (x < 0) {
-        console.log("swipeLeft");
         executeScript('document.body.style.backgroundColor = "yellow"');
+        executeScript('console.log("swipeLeft");');
     } else {
-        console.log("swipeRight");
         executeScript('document.body.style.backgroundColor = "blue"');
+        executeScript('console.log("swiprRight");');
     }
+}
+
+function clickToLink(){
+    var linkList;
+    executeScript('var linkList = document.getElementsByTagName("a")[1].href;');
+    executeScript('console.log("click to link");');
+    executeScript('console.log(linkList);');
+    executeScript('location.href = linkList;');
 }
 
 function clickFinger(){
