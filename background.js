3470c75fbe52ef6d3a8302d4cc5b062155a39da5
Kayawari Takayuki <kayamelo@gmail.com>
2015-06-04 10:19:26 +0900

    add function which "ClickToLink","linkRefresh"

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 var connected = false; // Boolean: { false: disconnected, true: connected }
 var gestureState = 0;  // Number: { 0: none, 1: start, 2: progress }
 var leap = null;       // Leap.Controller instance
+var takeLinksList = 0;//リンクを指の本数分取得するが、そのスタートの番号...
+
+//五本指操作を検討したプログラム
 
 chrome.browserAction.onClicked.addListener(function() {
     connected = !connected; // toggle state
@@ -30,7 +33,7 @@ function onframe(frame) {
         console.log("接続失敗");
         return;
     }
-    var i = 0
+    var i = 0;
     var hand = frame.hands[0];//hand class
     var finger = hand.fingers[0];//fingers class
     var thumbPointable = frame.pointables[0];//Pointable class
@@ -39,7 +42,7 @@ function onframe(frame) {
     var ringPointable = frame.pointables[3];
     var pinkyPointable = frame.pointables[4];
     //frame.pointable[5]以降は、2本目の手の指の数になる。
-
+    var fingersNum = 5;
     var iges = frame.gestures.length;
     var ifin = frame.fingers.length;//The number of th finger
 
@@ -51,17 +54,14 @@ function onframe(frame) {
             var middleSpeed = middlePointable.tipVelocity;
             var ringSpeed = ringPointable.tipVelocity;
             var pinkySpeed = pinkyPointable.tipVelocity;
-
-            //executeScript('console.log(' + thumbSpeed + ');');
                 
-            if(thumbSpeed[1] > 500) {clickToLink(0);}
-            if(indexSpeed[1] > 500) {clickToLink(1);}
-            if(middleSpeed[1] > 500) {clickToLink(2);}
-            if(ringSpeed[1] > 500) {clickToLink(3);}
-            if(pinkySpeed[1] > 500) {clickToLink(4);}
+            if(thumbSpeed[1] > 400) clickToLink(fingersNum,0);
+            if(indexSpeed[1] > 400) clickToLink(fingersNum,1); 
+            if(middleSpeed[1] > 400) clickToLink(fingersNum,2);
+            if(ringSpeed[1] > 400) clickToLink(fingersNum,3);
+            if(pinkySpeed[1] > 400) clickToLink(fingersNum,4);
             //マイナス方向も判定に入れる？　＋　正確な値にするべきかも。
-            
-            
+
             if(gesture.type === "swipe"){
                 switch(gesture.state){
                     case "start":
@@ -79,36 +79,54 @@ function onframe(frame) {
                             gestureState = 0;
                             swipe(  gesture.direction[0],
                                     gesture.direction[1],
-                                    gesture.direction[2]);
+                                    gesture.direction[2],
+                                    takeLinksList );
                         }
                 }
             }
+            /*
+            if(gesture.type === "circle"){
+                //これもswitchで、start, update, stopしたほうがいいのかな？
+                previousLinkSelecting(takeLinksList);
+            }
+            */
+            
         }
     }   
 
 }
 
-function swipe(x, y, z) {
+function swipe(x, y, z, listNum) {
+    executeScript('console.log("swipe!!");');
     if (x < 0) {
-        executeScript('document.body.style.backgroundColor = "yellow"');
-        executeScript('console.log("swipeLeft");');
+        executeScript('history.back();');
     } else {
-        executeScript('document.body.style.backgroundColor = "blue"');
-        executeScript('console.log("swiprRight");');
+        nextLinkSelecting( listNum );
     }
+    //これ、y,zは受け渡さなくても良くね？？？
 }
 
-function clickToLink(linkNum){
-    //最初から下一行みたいに、文字列連結すれば、値を送ることは可能。
-    executeScript('var linkList = document.getElementsByTagName("a")[' + linkNum + '].href;');
-    executeScript('console.log("click to link");');
-    executeScript('console.log(linkList);');
-    executeScript('location.href = linkList;');
+function clickToLink( fingersNum, fingersValue ){
+    executeScript('console.log("takeLinks!!");');
+    executeScript('var linkList = new Array();');
+    var i = takeLinksList;
+    while( i < takeLinksList + fingersNum){
+        executeScript('linkList.push(document.getElementsByTagName("a")[' + i + '].href);');
+        i++;
+    }
+    //executeScript('console.log(linkList);');
+    executeScript('location.href = linkList[' + fingersValue + '];');
 }
 
-function clickFinger(){
-    console.log('Link click');
-    executeScript('document.link[i]'); 
+function nextLinkSelecting( listNum ){
+    executeScript('console.log("Refresh next link!!");');
+    takeLinksList = listNum + 5;
+}
+
+//スワイプ左は、history.backに利用しているので、他のジェスチャが必要。
+function previousLinkSelecting( listNum ){
+    executeScript('console.log("Refresh previous link!!");');
+    takeLinksList = listNum - 5;
 }
 
 function executeScript(code) {
