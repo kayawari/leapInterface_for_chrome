72d5dc563ae04d3c3ca50aad7858d8cbd2975988
Kayawari Takayuki <kayamelo@gmail.com>
2015-10-09 10:27:19 +0900

    配列や音声案内タイミング微調整

--- background.js
+++ background.js
@@ -3,8 +3,8 @@ var takeLinksList   = 0;
 var takeHTagList    = 0;
 var takeDomSortList_1 = 0;
 var takeDomSortList_2 = 0;
-var takeSortList_1 =0;
-var takeSortList_2 =0;
+var takeSortList_1 = 0;
+var takeSortList_2 = 0;
 var formsObjects = {
     links       :[],
     imgAlt      :[],
@@ -46,6 +46,7 @@ chrome.browserAction.onClicked.addListener(function() {
 chrome.runtime.onMessage.addListener(function(req,sen,sendRes){
     if(req.keycode == 49){//type '1'
         clickSwitcher = 1;
+        takeLinksList = 0;
         chrome.storage.local.get(formsObjects,function(items){
             executeScript('console.log('+JSON.stringify(items)+');');
             //formsObjectsLinksLength = items.links.length;
@@ -53,7 +54,8 @@ chrome.runtime.onMessage.addListener(function(req,sen,sendRes){
         speechText_links(takeLinksList);
     }
     if(req.keycode == 50){//type '2'
-        clickSwitcher = 2
+        clickSwitcher = 2;
+        takeHTagList = 0;
         chrome.storage.local.get(hTagFormsObjects,function(items){
             executeScript('console.log('+JSON.stringify(items)+');');
             //formsObjectsLinksLength = items.links.length;
@@ -62,41 +64,48 @@ chrome.runtime.onMessage.addListener(function(req,sen,sendRes){
     }
     if(req.keycode == 51){//type '3'
         clickSwitcher = 3;
+        takeDomSortList_1 = 0;
+        selectedDomLayerNum = 0;
         chrome.storage.local.get(domSortFormsObjects_1,function(items){
             executeScript('console.log('+JSON.stringify(items)+');');
             //formsObjectsLinksLength = items.links.length;
             maxDomLayerNum = items.maxDomLayer;
         });
-        speechText_domSort(takeDomSortList_1);
+        speechText_normal('階層を選択してください。');
     }
     if(req.keycode == 52){//type '4'
         clickSwitcher = 4;
+        takeDomSortList_2 = 0;
+        selectedDomLayerNum = 0;
         chrome.storage.local.get(domSortFormsObjects_2,function(items){
             executeScript('console.log('+JSON.stringify(items)+');');
             //formsObjectsLinksLength = items.links.length;
             maxDomLayerNum = items.maxDomLayer;
-        });
-        speechText_domSort(takeDomSortList_2);
+        })
+        speechText_normal('階層を選択してください。');
     }
     if(req.keycode == 53){//type '5'
         clickSwitcher = 5;
+        takeSortList_1 = 0;
         chrome.storage.local.get(sortWithDom,function(items){
             executeScript('console.log('+JSON.stringify(items.sortWithDomObjects)+');');
         });
-        speechText_domSort(takeSortList_1);
+        speechText_sortLinks_1(takeSortList_1);
     }
     if(req.keycode == 54){//type '6'
         clickSwitcher = 6;
+        takeSortList_2 = 0;
         chrome.storage.local.get(sortWithDomAndTags,function(items){
             executeScript('console.log('+JSON.stringify(items.sortWithDomAndTagsObjects)+');');
         });
-        speechText_domSort(takeSortList_2);
+        speechText_sortLinks_2(takeSortList_2);
     }
     if(req.keycode == 55){//type '7'
         if(clickSwitcher == 3 || clickSwitcher == 4){
             minusDomLayerNum();
         }else{
             executeScript('console.log("DOM選択モードになっていません。");');
+            speechText_normal('DOM選択モードになっていません');
         }
     }
     if(req.keycode == 56){//type '8'
@@ -104,6 +113,7 @@ chrome.runtime.onMessage.addListener(function(req,sen,sendRes){
             plusDomLayerNum();
         }else{
             executeScript('console.log("DOM選択モードになっていません。");');
+            speechText_normal('DOM選択モードになっていません');
         }
     }
 
@@ -116,6 +126,8 @@ chrome.runtime.onMessage.addListener(function(req,sen,sendRes){
     }
     if(req.keycode == 90){//type 'Z'
         clickToLink();
+
+        //これがまだできてない。
     }
 });
 
@@ -140,128 +152,164 @@ function clickToLink(){
 }
 
 function plusDomLayerNum(){
+    takeDomSortList_1 = 0;
+    takeDomSortList_2 = 0;
     if(selectedDomLayerNum < maxDomLayerNum) {
         selectedDomLayerNum++;
-        executeScript('console.log("現在の階層は、' + selectedDomLayerNum + '番目です");');
+        speechText_normal('現在の階層は'+selectedDomLayerNum+'番目です');
     }else{
-        executeScript('console.log("これ以上、深い階層はありません。");');
+        speechText_normal('これ以上、深い階層はありません。');
     }
-
-    //音声案内も必要だろう。
-
 }
 
 function minusDomLayerNum(){
+    takeDomSortList_1 = 0;
+    takeDomSortList_2 = 0;
     if(selectedDomLayerNum > 0) {
         selectedDomLayerNum--;
-        executeScript('console.log("現在の階層は、' + selectedDomLayerNum + '番目です");');
+        speechText_normal('現在の階層は'+selectedDomLayerNum+'番目です');
     }else{
-        executeScript('console.log("最浅層です。");');
+        speechText_normal('一番浅い階層です。');
     }
-
-    //音声案内も
 }
 
 function nextLinkSelecting(){
-    executeScript('console.log("Refresh next link.");');
+    //executeScript('console.log("next link or text.");');
     switchSpeechText(1);
 }
 
 function previousLinkSelecting(){
-    executeScript('console.log("Refresh previous link.");');
-    if(takeLinksList > 0 || takeHTagList > 0 || takeDomSortList_1 > 0 || takeDomSortList_2 > 0 || takeSortList_1 > 0 || takeSortList_2 > 0){
-        switchSpeechText(-1);
-    }
+    //executeScript('console.log("previous link or text.");');
+    if( takeLinksList       > 0 || 
+        takeHTagList        > 0 || 
+        takeDomSortList_1   > 0 || 
+        takeDomSortList_2   > 0 || 
+        takeSortList_1      > 0 || 
+        takeSortList_2      > 0 ){ switchSpeechText(-1);}
 }
 
-function switchSpeechText(num){
-    if(clickSwitcher === 1) {
-        speechText_links(takeLinksList);
-        takeLinksList = takeLinksList + num;
-    }
-    if(clickSwitcher === 2) {
-        speechText_hTags(takeHTagList);
-        takeHTagList = takeHTagList + num;
-    }
-    if(clickSwitcher === 3){
-        speechText_domSort_1(takeDomSortList_1);
-        takeDomSortList_1 = takeDomSortList_1 + num;
-    }
-    if(clickSwitcher === 4){
-        speechText_domSort_2(takeDomSortList_2);
-        takeDomSortList_2 = takeDomSortList_2 + num;
-    }
-    if(clickSwitcher === 5){
-        speechText_sortLinks_1(takeSortList_1);
-        takeSortList_1 = takeSortList_1 + num;
-    }
-    if(clickSwitcher === 6){
-        speechText_sortLinks_2(takeSortList_2);
-        takeSortList_2 = takeSortList_2 + num;
-    }
+function switchSpeechText(selectNum){
+    //selectNumは、-1か+1のどちらか
+    if(clickSwitcher === 1) { speechText_links(selectNum); }
+    if(clickSwitcher === 2) { speechText_hTags(selectNum); }
+    if(clickSwitcher === 3) { speechText_domSort_1(selectNum); }
+    if(clickSwitcher === 4) { speechText_domSort_2(selectNum); }
+    if(clickSwitcher === 5) { speechText_sortLinks_1(selectNum); }
+    if(clickSwitcher === 6) { speechText_sortLinks_2(selectNum);}
 }
 
-function speechText_links(num){
+function speechText_normal(str){
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = str;
+    msg.lang = 'ja-JP';
+    executeScript('console.log("' + msg.text + '");');
+    speechSynthesis.speak(msg);
+}
+
+function speechText_links(selectNum){
     confirmStausOfSpeechsynthesis();
     chrome.storage.local.get(formsObjects,function(items){
+        if(takeLinksList < items.links.length && takeLinksList >= 0){
+            takeLinksList = takeLinksList + selectNum;
+        } else {
+            executeScript('console.log("'+takeLinksList+'")');
+            takeLinksList--;
+        }
         var msg = new SpeechSynthesisUtterance();
-        msg.text = items.links[num].text.toString();
+        msg.text = items.links[takeLinksList].text.toString();
         executeScript('console.log("' + msg.text + '");');
         msg.lang = 'ja-JP';
         speechSynthesis.speak(msg);
     });
 }
 
-function speechText_hTags(num){
+function speechText_hTags(selectNum){
     confirmStausOfSpeechsynthesis();
     chrome.storage.local.get(hTagFormsObjects,function(items){
-        var msg = new SpeechSynthesisUtterance(items.headTags[num].toString());
-        msg.text = items.headTags[num].toString();
+        if(takeHTagList < items.headTags.length && takeHTagList >= 0){
+            takeHTagList = takeHTagList + selectNum;
+        } else {
+            executeScript('console.log("'+takeHTagList+'")');
+            takeHTagList--;
+        }
+        var msg = new SpeechSynthesisUtterance();
+        msg.text = items.headTags[takeHTagList].toString();
         executeScript('console.log("' + msg.text + '");');
         msg.lang = 'ja-JP';
         speechSynthesis.speak(msg);
     });
 }
 
-function speechText_sortLinks_1(num){
+function speechText_sortLinks_1(selectNum){
     confirmStausOfSpeechsynthesis();
     chrome.storage.local.get(sortWithDom,function(items){
-        var msg = new SpeechSynthesisUtterance(items.sortWithDomObjects[num].text.toString());
-        msg.text = items.sortWithDomObjects[num].text.toString();
+        if(takeDomSortList_1 < items.sortWithDomObjects.length && takeDomSortList_1 >= 0){
+            takeDomSortList_1 = takeDomSortList_1 + selectNum;
+        } else {
+            executeScript('console.log("'+takeDomSortList_1+'")');
+            takeDomSortList_1--;
+        }
+        var msg = new SpeechSynthesisUtterance();
+        msg.text = items.sortWithDomObjects[takeDomSortList_1].text.toString();
         executeScript('console.log("' + msg.text + '");');
         msg.lang = 'ja-JP';
         speechSynthesis.speak(msg);
     });
 }
 
-function speechText_sortLinks_2(num){
+function speechText_sortLinks_2(selectNum){
     confirmStausOfSpeechsynthesis();
     chrome.storage.local.get(sortWithDomAndTags,function(items){
-        var msg = new SpeechSynthesisUtterance(items.sortWithDomAndTagsObjects[num].text.toString());
-        msg.text = items.sortWithDomAndTagsObjects[num].text.toString();
+        if(takeDomSortList_2 < items.sortWithDomAndTagsObjects.length && takeDomSortList_2 >= 0){
+            takeDomSortList_2 = takeDomSortList_2 + selectNum;
+        } else {
+            executeScript('console.log("'+takeDomSortList_2+'")');
+            takeDomSortList_2--;
+        }
+        var msg = new SpeechSynthesisUtterance();
+        msg.text = items.sortWithDomAndTagsObjects[takeDomSortList_2].text.toString();
         executeScript('console.log("' + msg.text + '");');
         msg.lang = 'ja-JP';
         speechSynthesis.speak(msg);
     });
 }
 
-//発声させるためのキーボードキーを入れてない。
-function speechText_domSort_1(num){
+function speechText_domSort_1(selectNum){
     confirmStausOfSpeechsynthesis();
     chrome.storage.local.get(domSortFormsObjects_1,function(items){
-        var msg = new SpeechSynthesisUtterance(items.DOMObjects[selectedDomLayerNum][num].text.toString());
-        msg.text = items.DOMObjects[selectedDomLayerNum][num].text.toString();
+        if(items.DOMObjects[selectedDomLayerNum].length == 0){
+            speechText_normal('この階層にリンクはありません');
+            return;
+        }
+        if(takeSortList_1 < items.DOMObjects[selectedDomLayerNum].length && takeSortList_1 >= 0){
+            takeSortList_1 = takeSortList_1 + selectNum;
+        } else {
+            executeScript('console.log("'+takeSortList_1+'")');
+            takeSortList_1--;
+        }
+        var msg = new SpeechSynthesisUtterance();
+        msg.text = items.DOMObjects[selectedDomLayerNum][takeSortList_1].text.toString();
         executeScript('console.log("' + msg.text + '");');
         msg.lang = 'ja-JP';
         speechSynthesis.speak(msg);
     });
 }
 
-function speechText_domSort_2(num){
+function speechText_domSort_2(selectNum){
     confirmStausOfSpeechsynthesis();
     chrome.storage.local.get(domSortFormsObjects_2,function(items){
-        var msg = new SpeechSynthesisUtterance(items.DOMObjects[selectedDomLayerNum][num].text.toString());
-        msg.text = items.DOMObjects[selectedDomLayerNum][num].text.toString();
+        if(items.DOMObjects[selectedDomLayerNum].length == 0){
+            speechText_normal('この階層にリンクはありません');
+            return;
+        }
+        if(takeSortList_2 < items.DOMObjects[selectedDomLayerNum].length && takeSortList_2 >= 0){
+            takeSortList_2 = takeSortList_2 + selectNum;
+        } else {
+            executeScript('console.log("'+takeSortList_2+'")');
+            takeSortList_2--;
+        }
+        var msg = new SpeechSynthesisUtterance();
+        msg.text = items.DOMObjects[selectedDomLayerNum][takeSortList_2].text.toString();
         executeScript('console.log("' + msg.text + '");');
         msg.lang = 'ja-JP';
         speechSynthesis.speak(msg);
@@ -280,4 +328,3 @@ function executeScript(code){
         code:code
     });
 }
-
