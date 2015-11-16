d152903ae45b1f9cb2d7ec250fba6ce965493cab
Kayawari Takayuki <kayamelo@gmail.com>
2015-11-16 18:11:31 +0900

    clickToLink()は実験で不要のため削除

--- background.js
+++ background.js
@@ -77,43 +77,6 @@ chrome.runtime.onMessage.addListener(function(req,sen,sendRes){
     }
 });
 
-function clickToLink(){
-	if(clickSwitcher == 2) return 0;
-	if(clickFlag){
-		clickFlag = false;
-		var speechText = new SpeechSynthesisUtterance();
-		speechText.lang = 'JP-ja';
-
-		if(clickSwitcher == 1){
-			var clickedURINumber = takeSortList_2;
-			chrome.storage.local.get(takeSortList_2,function(items){
-				var clickedURIText  = items.DOMObjects[selectedDomLayerNum][clickedURINumber].text;
-				var clickedURI      = items.DOMObjects[selectedDomLayerNum][clickedURINumber].uri;
-				speechText.text = clickedURIText.toString();
-				speechSynthesis.speak(speechText);
-				executeScript('location.href="' + clickedURI.toString() + '";');
-			});
-		}
-
-		if(clickSwitcher == 2){
-			var clickedURINumber = takeDomSortList_2;
-			chrome.storage.local.get(takeDomSortList_2,function(items){
-				var clickedURIText  = items.sortWithDomAndTagsObjects[selectedDomLayerNum][clickedURINumber].text;
-				var clickedURI      = items.sortWithDomAndTagsObjects[selectedDomLayerNum][clickedURINumber].uri;
-				speechText.text = clickedURIText.toString();
-				speechSynthesis.speak(speechText);
-				executeScript('location.href="' + clickedURI.toString() + '";');
-			});
-		}
-
-		speechText.onend = function(e){
-			clickFlag = true;
-		};
-
-	}
-
-}
-
 function plusDomLayerNum(){
 	takeSortList_2 = 0;
 	if(selectedDomLayerNum < maxDomLayerNum) {
