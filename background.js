84a6e80b2f80b4c9685b6c581e7ad25b0a38831c
Kayawari Takayuki <kayamelo@gmail.com>
2015-11-16 17:08:47 +0900

    最終実験用に不要機能削除

--- background.js
+++ background.js
@@ -1,40 +1,24 @@
 //jquery 2.x.x not supporting IE 6,7,8
-var takeLinksList   = 0;
-var takeHTagList    = 0;
-var takeDomSortList_1 = 0;
 var takeDomSortList_2 = 0;
-var takeSortList_1 = 0;
 var takeSortList_2 = 0;
 
-var formsObjects = {
-    links       :[]
-};
-var hTagFormsObjects = {
-    headTags    :[]
-};
-var domSortFormsObjects_1 = {
-    DOMObjects :[],
-    maxDomLayer:0
-};
+//chrome.local.storageのJSONフォーマット
+//下二つのオブジェクト。なぜか形式を変えないと上書きされてしまうので、tmpNum,Numを無理やり追加
 var domSortFormsObjects_2 = {
     DOMObjects :[],
     maxDomLayer:0
 };
-
-//下二つのオブジェクト。なぜか形式を変えないと上書きされてしまうので、tmpNum,Numを無理やり追加
-var sortWithDom = {
-    sortWithDomObjects : [],
-    tmpNum : 0
-};
 var sortWithDomAndTags = {
-    Num : 0,
     sortWithDomAndTagsObjects : []
 };
 
-//var formsObjectsLinksLength = 0;
 var clickFlag = true;
+
+//クリックされたキーを識別する変数
 var clickSwitcher = 1;
+//dom階層選択変数
 var selectedDomLayerNum = 1;
+//取得階層の最大値
 var maxDomLayerNum = 0;
 
 chrome.browserAction.onClicked.addListener(function() {
@@ -44,42 +28,10 @@ chrome.browserAction.onClicked.addListener(function() {
 
 chrome.runtime.onMessage.addListener(function(req,sen,sendRes){
     if(req.keycode == 49){//type '1'
-        clickSwitcher = 1;
-        takeLinksList = 0;
-        chrome.storage.local.get(formsObjects,function(items){
-            executeScript('console.log('+JSON.stringify(items)+');');
-            //formsObjectsLinksLength = items.links.length;
-        });
-        speechText_links(takeLinksList);
-    }
-    if(req.keycode == 50){//type '2'
-        clickSwitcher = 2;
-        takeHTagList = 0;
-        chrome.storage.local.get(hTagFormsObjects,function(items){
-            executeScript('console.log('+JSON.stringify(items)+');');
-            //formsObjectsLinksLength = items.links.length;
-        });
-        speechText_hTags(takeHTagList);
-    }
-    if(req.keycode == 51){//type '3'
-        clickSwitcher = 3;
-        takeDomSortList_1 = 0;
-        selectedDomLayerNum = 0;
-        chrome.storage.local.get(domSortFormsObjects_1,function(items){
-            executeScript('console.log('+JSON.stringify(items)+');');
-            //formsObjectsLinksLength = items.links.length;
-            maxDomLayerNum = items.maxDomLayer;
-        });
-        speechText_normal('階層を選択してください。');
-        speechText_normal(maxDomLayerNum + '階層あります。');
-    }
-    if(req.keycode == 52){//type '4'
-        clickSwitcher       = 4;
-        //クリックされたキーを識別する変数
+        clickSwitcher       = 1;
         takeDomSortList_2   = 0;
         //階層内のリンクを選ぶ変数をキーを押した段階でリセット
         selectedDomLayerNum = 0;
-        //dom階層選択変数
         chrome.storage.local.get(domSortFormsObjects_2,function(items){
             executeScript('console.log('+JSON.stringify(items)+');');
             maxDomLayerNum = items.maxDomLayer;
@@ -88,41 +40,30 @@ chrome.runtime.onMessage.addListener(function(req,sen,sendRes){
         speechText_normal('階層を選択してください。');
         speechText_normal(maxDomLayerNum + '階層あります。');
     }
-
-    if(req.keycode == 53){//type '5'
-        clickSwitcher = 5;
-        takeDomSortList_1 = 0;
-        chrome.storage.local.get(sortWithDom,function(items){
-            executeScript('console.log('+JSON.stringify(items.sortWithDomObjects)+');');
-        });
-        speechText_sortLinks_1(takeDomSortList_1);
-    }
-    if(req.keycode == 54){//type '6'
-        clickSwitcher = 6;
+    if(req.keycode == 50){//type '2'
+        clickSwitcher = 2;
         takeDomSortList_2 = 0;
         chrome.storage.local.get(sortWithDomAndTags,function(items){
             executeScript('console.log('+JSON.stringify(items.sortWithDomAndTagsObjects)+');');
         });
         speechText_sortLinks_2(takeDomSortList_2);
     }
-    if(req.keycode == 55){//type '7'
-        if(clickSwitcher == 3 || clickSwitcher == 4){
+    if(req.keycode == 51){//type '3'
+        if(clickSwitcher == 1){
             minusDomLayerNum();
         }else{
             executeScript('console.log("DOM選択モードになっていません。");');
             speechText_normal('DOM選択モードになっていません');
         }
     }
-    if(req.keycode == 56){//type '8'
-        if(clickSwitcher == 3 || clickSwitcher == 4){
+    if(req.keycode == 52){//type '4'
+        if(clickSwitcher == 1){
             plusDomLayerNum();
         }else{
             executeScript('console.log("DOM選択モードになっていません。");');
             speechText_normal('DOM選択モードになっていません');
         }
     }
-
-
     if(req.keycode == 69){//type 'E'
         previousLinkSelecting();
     }
@@ -143,29 +84,7 @@ function clickToLink(){
        var speechText = new SpeechSynthesisUtterance();
        speechText.lang = 'JP-ja';
 
-       if(clickSwitcher == 1){
-                 var clickedURINumber = takeLinksList;
-                 chrome.storage.local.get(formsObjects,function(items){
-                    var clickedURIText  = items.links[clickedURINumber].text;
-                    var clickedURI      = items.links[clickedURINumber].uri;
-                    speechText.text = clickedURIText.toString();
-                    speechSynthesis.speak(speechText);
-                    executeScript('location.href="' + clickedURI.toString() + '";');
-                 });
-        }
- 
-       if(clickSwitcher == 3){
-                 var clickedURINumber = takeSortList_1;
-                 chrome.storage.local.get(takeSortList_1,function(items){
-                    var clickedURIText  = items.DOMObjects[selectedDomLayerNum][clickedURINumber].text;
-                    var clickedURI      = items.DOMObjects[selectedDomLayerNum][clickedURINumber].uri;
-                    speechText.text = clickedURIText.toString();
-                    speechSynthesis.speak(speechText);
-                    executeScript('location.href="' + clickedURI.toString() + '";');
-                 });
-        }
-
-         if(clickSwitcher == 4){
+         if(clickSwitcher == 1){
                  var clickedURINumber = takeSortList_2;
                  chrome.storage.local.get(takeSortList_2,function(items){
                     var clickedURIText  = items.DOMObjects[selectedDomLayerNum][clickedURINumber].text;
@@ -176,17 +95,7 @@ function clickToLink(){
                  });
          }
 
-         if(clickSwitcher == 5){
-                 var clickedURINumber = takeDomSortList_1;
-                 chrome.storage.local.get(takeDomSortList_1,function(items){
-                    var clickedURIText  = items.sortWithDomObjects[selectedDomLayerNum][clickedURINumber].text;
-                    var clickedURI      = items.sortWithDomObjects[selectedDomLayerNum][clickedURINumber].uri;
-                    speechText.text = clickedURIText.toString();
-                    speechSynthesis.speak(speechText);
-                    executeScript('location.href="' + clickedURI.toString() + '";');
-                 });
-         }
-         if(clickSwitcher == 6){
+         if(clickSwitcher == 2){
                  var clickedURINumber = takeDomSortList_2;
                  chrome.storage.local.get(takeDomSortList_2,function(items){
                     var clickedURIText  = items.sortWithDomAndTagsObjects[selectedDomLayerNum][clickedURINumber].text;
@@ -207,7 +116,6 @@ function clickToLink(){
 
 function plusDomLayerNum(){
     takeDomSortList_1 = 0;
-    takeDomSortList_2 = 0;
     if(selectedDomLayerNum < maxDomLayerNum) {
         selectedDomLayerNum++;
         speechText_normal('現在の階層は'+selectedDomLayerNum+'番目です');
@@ -217,7 +125,6 @@ function plusDomLayerNum(){
 }
 
 function minusDomLayerNum(){
-    takeDomSortList_1 = 0;
     takeDomSortList_2 = 0;
     if(selectedDomLayerNum > 0) {
         selectedDomLayerNum--;
@@ -234,22 +141,15 @@ function nextLinkSelecting(){
 
 function previousLinkSelecting(){
     //executeScript('console.log("previous link or text.");');
-    if( takeLinksList       > 0 || 
-        takeHTagList        > 0 || 
-        takeDomSortList_1   > 0 || 
-        takeDomSortList_2   > 0 || 
-        takeSortList_1      > 0 || 
-        takeSortList_2      > 0 ){ switchSpeechText(-1);}
+    if( takeDomSortList_2 > 0 || takeSortList_2 > 0 ){
+        switchSpeechText(-1);
+    }
 }
 
 function switchSpeechText(selectNum){
     //selectNumは、-1か+1のどちらか
-    if(clickSwitcher === 1) { speechText_links(selectNum); }
-    if(clickSwitcher === 2) { speechText_hTags(selectNum); }
-    if(clickSwitcher === 3) { speechText_domSort_1(selectNum); }
-    if(clickSwitcher === 4) { speechText_domSort_2(selectNum); }
-    if(clickSwitcher === 5) { speechText_sortLinks_1(selectNum); }
-    if(clickSwitcher === 6) { speechText_sortLinks_2(selectNum);}
+    if(clickSwitcher === 1) { speechText_domSort_2(selectNum); }
+    if(clickSwitcher === 2) { speechText_sortLinks_2(selectNum);}
 }
 
 function speechText_normal(str){
@@ -258,58 +158,6 @@ function speechText_normal(str){
     msg.lang = 'ja-JP';
     executeScript('console.log("' + msg.text + '");');
     speechSynthesis.speak(msg);
-}
-
-function speechText_links(selectNum){
-    confirmStausOfSpeechsynthesis();
-
-    chrome.storage.local.get(formsObjects,function(items){
-        if(takeLinksList < items.links.length && takeLinksList >= 0){
-            takeLinksList = takeLinksList + selectNum;
-        } else {
-            executeScript('console.log("'+takeLinksList+'")');
-            takeLinksList--;
-        }
-        var msg = new SpeechSynthesisUtterance();
-        msg.text = items.links[takeLinksList].text.toString();
-        executeScript('console.log("' + msg.text + '");');
-        msg.lang = 'ja-JP';
-        speechSynthesis.speak(msg);
-    });
-}
-
-function speechText_hTags(selectNum){
-    confirmStausOfSpeechsynthesis();
-    chrome.storage.local.get(hTagFormsObjects,function(items){
-        if(takeHTagList < items.headTags.length && takeHTagList >= 0){
-            takeHTagList = takeHTagList + selectNum;
-        } else {
-            executeScript('console.log("'+takeHTagList+'")');
-            takeHTagList--;
-        }
-        var msg = new SpeechSynthesisUtterance();
-        msg.text = items.headTags[takeHTagList].toString();
-        executeScript('console.log("' + msg.text + '");');
-        msg.lang = 'ja-JP';
-        speechSynthesis.speak(msg);
-    });
-}
-
-function speechText_sortLinks_1(selectNum){
-    confirmStausOfSpeechsynthesis();
-    chrome.storage.local.get(sortWithDom,function(items){
-        if(takeDomSortList_1 < items.sortWithDomObjects.length && takeDomSortList_1 >= 0){
-            takeDomSortList_1 = takeDomSortList_1 + selectNum;
-        } else {
-            executeScript('console.log("'+takeDomSortList_1+'")');
-            takeDomSortList_1--;
-        }
-        var msg = new SpeechSynthesisUtterance();
-        msg.text = items.sortWithDomObjects[takeDomSortList_1].text.toString();
-        executeScript('console.log("' + msg.text + '");');
-        msg.lang = 'ja-JP';
-        speechSynthesis.speak(msg);
-    });
 }
 
 function speechText_sortLinks_2(selectNum){
@@ -323,27 +171,6 @@ function speechText_sortLinks_2(selectNum){
         }
         var msg = new SpeechSynthesisUtterance();
         msg.text = items.sortWithDomAndTagsObjects[takeDomSortList_2].text.toString();
-        executeScript('console.log("' + msg.text + '");');
-        msg.lang = 'ja-JP';
-        speechSynthesis.speak(msg);
-    });
-}
-
-function speechText_domSort_1(selectNum){
-    confirmStausOfSpeechsynthesis();
-    chrome.storage.local.get(domSortFormsObjects_1,function(items){
-        if(items.DOMObjects[selectedDomLayerNum].length == 0){
-            speechText_normal('この階層にリンクはありません');
-            return;
-        }
-        if(takeSortList_1 < items.DOMObjects[selectedDomLayerNum].length && takeSortList_1 >= 0){
-            takeSortList_1 = takeSortList_1 + selectNum;
-        } else {
-            executeScript('console.log("'+takeSortList_1+'")');
-            takeSortList_1--;
-        }
-        var msg = new SpeechSynthesisUtterance();
-        msg.text = items.DOMObjects[selectedDomLayerNum][takeSortList_1].text.toString();
         executeScript('console.log("' + msg.text + '");');
         msg.lang = 'ja-JP';
         speechSynthesis.speak(msg);
--- search.js
+++ search.js
@@ -1,199 +1,70 @@
 $(function(){
-    //はじめにフォームタグなどを削除
-    removeInputTags();
-    var linksArray      = removeSameLinkArray();
-    var formObjects = {
-        links       :linksArray
-    };
-    chrome.storage.local.set(formObjects,function(){
-        console.log("%cset form objects to local storage !!","color:green;");
-    });
-
-    var headTagsArray   = extractHeadTagsArray();
-    var formObjects = {
-        headTags    :headTagsArray
-    };
-    chrome.storage.local.set(formObjects,function(){
-        console.log("%cset hTag objects to local storage !!","color:blue;");
-    });
-
-    var topNode = $('body');
-    var all_arr = extractDOMTree(topNode,1);
-    var sortWithDomLayersArray = sortWithDomLayers(all_arr);
-    var sortWithDomLayersAndObjectsArray = sortWithDomLayersAndObjects(all_arr);
-    var sortFormObject_1 = {
-        DOMObjects : sortWithDomLayersArray,
-        maxDomLayer: maxDom
-    };
-    var sortFormObject_2 = {
-        DOMObjects : sortWithDomLayersAndObjectsArray,
-        maxDomLayer: maxDom
-    };
-    chrome.storage.local.set(sortFormObject_1,function(){
-        console.log("%cset link objects 1 with DOM to local storage !!","color:red;");
-    });
-    chrome.storage.local.set(sortFormObject_2,function(){
-        console.log("%cset link objects 2 with DOM to local storage !!","color:pink;");
-    });
-
-    var sortWithDomLayersObjects = extractObjectsFromArray(sortWithDomLayersArray);
-    var sortWithDomAndTagsObjects = extractObjectsFromArray(sortWithDomLayersAndObjectsArray);
-    var sortWithDom = {
-        sortWithDomObjects : sortWithDomLayersObjects,
-        tmpNum : 1
-    };
-    var sortWithDomAndTags = {
-        Num : 2,
-        sortWithDomAndTagsObjects : sortWithDomAndTagsObjects
-    };
-    chrome.storage.local.set(sortWithDom,function(){
-        console.log("%cset link Array with DOM to local storage !!","color:yellow;");
-    });
-    chrome.storage.local.set(sortWithDomAndTags,function(){
-        console.log("%cset link Array with DOM to local storage !!","color:skyblue;");
-    });
+	var topNode = $('body');
+	var all_arr = extractDOMTree(topNode,1);
+	var sortWithDomLayersAndObjectsArray = sortWithDomLayersAndObjects(all_arr);
+	
+	chrome.storage.local.set({ DOMObjects : sortWithDomLayersAndObjectsArray,maxDomLayer: maxDom},function(){
+		console.log("%cset link objects 2 with DOM to local storage !!","color:red;");
+	});
+	
+	chrome.storage.local.set({ sortWithDomAndTagsObjects : extractObjectsFromArray(sortWithDomLayersAndObjectsArray) },function(){
+		console.log("%cset link Array with DOM to local storage !!","color:blue;");
+	});
 });
 
 $(window).keydown(function(e){
-    e.preventDefault();
+	e.preventDefault();
 
-    if(e.keyCode == 49){
-        chrome.runtime.sendMessage({keycode:49},function(response){
+	if(e.keyCode == 49){
+		chrome.runtime.sendMessage({keycode:49},function(response){
            //console.log('type 1');
-        });
-    }
-    if(e.keyCode == 50){
-        chrome.runtime.sendMessage({keycode:50},function(response){
+       });
+	}
+	if(e.keyCode == 50){
+		chrome.runtime.sendMessage({keycode:50},function(response){
             //console.log('type 2');
         });
-    }
-    if(e.keyCode == 51){
-        chrome.runtime.sendMessage({keycode:51},function(response){
+	}
+	if(e.keyCode == 51){
+		chrome.runtime.sendMessage({keycode:51},function(response){
             //console.log('type 3');
         });
-    }
-    if(e.keyCode == 52){
-        chrome.runtime.sendMessage({keycode:52},function(response){
+	}
+	if(e.keyCode == 52){
+		chrome.runtime.sendMessage({keycode:52},function(response){
             //console.log('type 4');
         });
-    }
-    if(e.keyCode == 53){
-        chrome.runtime.sendMessage({keycode:53},function(response){
-            //console.log('type 5');
-        });
-    }
-    if(e.keyCode == 54){
-        chrome.runtime.sendMessage({keycode:54},function(response){
-            //console.log('type 6');
-        });
-    }
-    if(e.keyCode == 55){
-        chrome.runtime.sendMessage({keycode:55},function(response){
-            //console.log('type 7');
-        });
-    }
-    if(e.keyCode == 56){
-        chrome.runtime.sendMessage({keycode:56},function(response){
-            //console.log('type 8');
-        });
-    }
-
-    if(e.keyCode == 69){
-        chrome.runtime.sendMessage({keycode:69},function(response){
+	}
+	
+	if(e.keyCode == 69){
+		chrome.runtime.sendMessage({keycode:69},function(response){
             //console.log('type E');
         });
-    }
-    if(e.keyCode == 82){
-        chrome.runtime.sendMessage({keycode:82},function(response){
+	}
+	if(e.keyCode == 82){
+		chrome.runtime.sendMessage({keycode:82},function(response){
             //console.log('type R');
         });
-    }
-    if(e.keyCode == 90){
-        chrome.runtime.sendMessage({keycode:90},function(response){
+	}
+	if(e.keyCode == 90){
+		chrome.runtime.sendMessage({keycode:90},function(response){
             //console.log('type Z');
         });
-    }
+	}
 
 });
 
-function removeInputTags(){
-    $('input,select,form,textarea').each(function(number){
-        $(this).remove();
-    });
-}
-
-function removeSameLinkArray(){
-    var linkList = [];
-    $("a").each(function(){
-        linkList.push({
-            "uri"   :$(this).attr("href"),
-            "text"  :$(this).text(),
-            "title" :$(this).attr("title")
-        });
-    });
-    
-    //リンクの重複削除
-    var arrObj = {};
-    for(var i=0; i<linkList.length; i++){
-        arrObj[linkList[i]['uri']] = linkList[i];
-    }
-    linkList = [];
-    for(var key in arrObj){
-        linkList.push(arrObj[key]);
-    }
-    
-    //textがないリンク削除
-    for(var i=0; i<linkList.length; i++){
-        if(!linkList[i].text || linkList[i].text === null || linkList[i].text === undefined || linkList[i].text.length == 0 || linkList[i].text === ""){
-            linkList.splice(i,1);
-            i--;
-        }
-    }
-
-    return sortLinkByLengthOfTheSentence(linkList);
-}
-
-function extractHeadTagsArray(){
-    var headTagsArray = [];
-    $('h1,h2,h3,h4,h5,h6').each(function(){
-        headTagsArray.push( $(this).text() );
-    });
-
-    //textがないhタグ削除
-    for(var i=0; i<headTagsArray.length; i++){
-        if(!headTagsArray[i] || headTagsArray[i] === null || headTagsArray[i] === undefined || headTagsArray[i].length == 0 || headTagsArray[i] === ""){
-            headTagsArray.splice(i,1);
-            i--;
-        }
-    }
-
-    return sortHTagByLengthOfTheSentence(headTagsArray);
-}
-
-function sortHTagByLengthOfTheSentence(processedList){
-    var listLength = processedList.length;
-    var temp;
-    for(var i=0; i<listLength-1; i++){
-        for(var j=listLength-1; j>i; j--){
-            if( processedList[j-1].length > processedList[j].length ){
-                temp = processedList[j-1];
-                processedList[j-1] = processedList[j];
-                processedList[j] = temp;
-            }
-        }
-    }
-    return processedList;
-}
-
+//配列・オブジェクトの中身のテキストの長さの昇順に並び替え
+//クイックソートにしたい
 function sortLinkByLengthOfTheSentence(processedList){
-    var listLength = processedList.length;
-    var temp;
-    for(var i=0; i<listLength-1; i++){
-        for(var j=listLength-1; j>i; j--){
-            if( processedList[j-1].text.length > processedList[j].text.length ){
-                temp = processedList[j-1];
-                processedList[j-1] = processedList[j];
-                processedList[j] = temp;
+	var listLength = processedList.length;
+	var temp;
+	for(var i=0; i<listLength-1; i++){
+		for(var j=listLength-1; j>i; j--){
+			if( processedList[j-1].text.length > processedList[j].text.length ){
+				temp = processedList[j-1];
+				processedList[j-1] = processedList[j];
+				processedList[j] = temp;
                 //bubble sort
             }
         }
@@ -201,113 +72,99 @@ function sortLinkByLengthOfTheSentence(processedList){
     return processedList;
 }
 
+//DOMに応じて、Webページのリンク情報を抽出
 var all_arr = [];
 var maxDom = 0;//DOMの最深層の数を格納
 function extractDOMTree(nodeObject,layerNum){
-    var temp_arr = [];
-    var nodeObjectChildren = nodeObject.children();
-    var nodeLength = nodeObjectChildren.length;
+	var temp_arr = [];
+	var nodeObjectChildren = nodeObject.children();
+	var nodeLength = nodeObjectChildren.length;
 
-    for(var i=0;i<nodeLength; i++){
-        var tag = $(nodeObjectChildren[i]).get(0).tagName;
+	for(var i=0;i<nodeLength; i++){
+		var tag = $(nodeObjectChildren[i]).get(0).tagName;
         //console.log(tag);
 
         if(tag == 'a' || tag == 'A'){
-            temp_arr.push({
-                text: $(nodeObjectChildren[i]).text(),
-                uri : $(nodeObjectChildren[i]).attr('href')
-            });
+        	temp_arr.push({
+        		text: $(nodeObjectChildren[i]).text(),
+        		uri : $(nodeObjectChildren[i]).attr('href')
+        	});
         }
     }
     if(temp_arr.length != 0){
-        all_arr.push(hash_key(layerNum,temp_arr));
+    	all_arr.push(hash_key(layerNum,temp_arr));
     }
 
     for(var j=0;j<nodeLength;j++){
-        if(nodeObjectChildren){
-            extractDOMTree($(nodeObjectChildren[j]),layerNum + 1);
-        }
+    	if(nodeObjectChildren){
+    		extractDOMTree($(nodeObjectChildren[j]),layerNum + 1);
+    	}
     }
     if(maxDom < layerNum) maxDom = layerNum;
     //console.log(maxDom);
-    return divideObjectsWithDom(all_arr);
+    return divideTheObjectsForEachOfDom(all_arr);
 }
 
 //ハッシュのkeyに変数を利用するための関数
-//ES6形式に変更？
+//ES6形式の場合、この関数は不要のはず
 function hash_key(key,value){
-    var h = {};
-    h[key] = value;
-    return h;
+	var h = {};
+	h[key] = value;
+	return h;
 }
 
 //階層毎に分ける
-function divideObjectsWithDom(arr){
-    var arrLength = arr.length;
-    var tmp_all_arr = [];
-    for(var domNum = 1; domNum <= maxDom; domNum++){
-        var tmpArr = [];
-        for(var i=0;i<arrLength;i++){
-            for(key in arr[i]){
-                if(key == domNum){
-                    tmpArr.push(arr[i]);
-                }
-            }
-        }
-        tmp_all_arr.push(tmpArr);
-    }
-    return tmp_all_arr;
+function divideTheObjectsForEachOfDom(arr){
+	var arrLength = arr.length;
+	var tmp_all_arr = [];
+	for(var domNum = 1; domNum <= maxDom; domNum++){
+		var tmpArr = [];
+		for(var i=0;i<arrLength;i++){
+			for(key in arr[i]){
+				if(key == domNum){
+					tmpArr.push(arr[i]);
+				}
+			}
+		}
+		tmp_all_arr.push(tmpArr);
+	}
+	return tmp_all_arr;
 }
 
 
 //ここがひどい.for文のネストがすごい事に.extractedDomTree()に盛り込めるはず。
-function sortWithDomLayers(arr){
-    var tmp_all_arr = [];
-    for(var i=0;i < arr.length; i++){
-        var tmpArr = [];
-        for(var j=0;j<arr[i].length;j++){
-            for(key in arr[i][j]){
-                for(var k=0; k<arr[i][j][key].length; k++){
-                    tmpArr.push(arr[i][j][key][k]);
-                }
-            }
-        }
-        sortLinkByLengthOfTheSentence(tmpArr);
-        tmp_all_arr.push(tmpArr);
-    }
-    return tmp_all_arr;
-}
-
+//domとtagの中身ごとにソート
 function sortWithDomLayersAndObjects(arr){
-    var tmp_all_arr = [];
-    for(var i=0;i < arr.length; i++){
-        var tmpArr = [];
-        for(var j=0;j<arr[i].length;j++){
-            for(key in arr[i][j]){
-                sortLinkByLengthOfTheSentence(arr[i][j][key]);
-                for(var k=0; k<arr[i][j][key].length; k++){
-                    tmpArr.push(arr[i][j][key][k]);
-                }
-            }
-        }
-        tmp_all_arr.push(tmpArr);
-    }
-    return tmp_all_arr;
+	var tmp_all_arr = [];
+	for(var i=0;i < arr.length; i++){
+		var tmpArr = [];
+		for(var j=0;j<arr[i].length;j++){
+			for(key in arr[i][j]){
+				sortLinkByLengthOfTheSentence(arr[i][j][key]);
+				for(var k=0; k<arr[i][j][key].length; k++){
+					tmpArr.push(arr[i][j][key][k]);
+				}
+			}
+		}
+		tmp_all_arr.push(tmpArr);
+	}
+	return tmp_all_arr;
 }
 
+//配列のオブジェクトのネストがすごかったので、配列の中のオブジェクトを出す。
 function extractObjectsFromArray(arr){
-    var tmp_all_arr = [];
-    for(var i=0;i<arr.length;i++){
-        for(var j=0;j<arr[i].length;j++){
-            tmp_all_arr.push(arr[i][j]);
-        }
-    }
-    
-    for(var i=0; i<tmp_all_arr.length; i++){
-        if(!tmp_all_arr[i].text || tmp_all_arr[i].text === null || tmp_all_arr[i].text === undefined || tmp_all_arr[i].text.length == 0 || tmp_all_arr[i].text === ""){
-            tmp_all_arr.splice(i,1);
-            i--;
-        }
-    }
-    return tmp_all_arr;
+	var tmp_all_arr = [];
+	for(var i=0;i<arr.length;i++){
+		for(var j=0;j<arr[i].length;j++){
+			tmp_all_arr.push(arr[i][j]);
+		}
+	}
+	
+	for(var i=0; i<tmp_all_arr.length; i++){
+		if(!tmp_all_arr[i].text || tmp_all_arr[i].text === null || tmp_all_arr[i].text === undefined || tmp_all_arr[i].text.length == 0 || tmp_all_arr[i].text === ""){
+			tmp_all_arr.splice(i,1);
+			i--;
+		}
+	}
+	return tmp_all_arr;
 }
